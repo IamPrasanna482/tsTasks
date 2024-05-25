72c0ee2ec547f104cd30f10441202e74ec1e3390
Prasanna Agnihotri <prasannagnihotri24@gmail.com>
2024-05-25 22:43:42 +0530

    Update task10706.ts

--- tasks/task10706.ts
+++ tasks/task10706.ts
@@ -20,12 +20,13 @@ const add = (a: number, b: number): number => a + b;
 
 // Higher-order function to wrap a function with logging
 
-const loggerWrapper = <T extends (...args: any[]) => any>(func: T) => {
-  return (...args: Parameters<T>): ReturnType<T> => {
+const loggerWrapper = (func: (...args: any[]) => any) => {
+  return (...args: any[]): any => {
     console.log(`${func.name} is called with arguments:`, ...args);
     return func(...args);
   };
 };
+
 
 // pass the add function to loggerWrappper function
 const loggedAdd = loggerWrapper(add);

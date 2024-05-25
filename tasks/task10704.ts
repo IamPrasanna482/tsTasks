6656414c684a3ebeb1cc88ed8f015f2f280c6d08
Prasanna Agnihotri <prasannagnihotri24@gmail.com>
2024-05-25 22:41:59 +0530

    Update task10704.ts

--- tasks/task10704.ts
+++ tasks/task10704.ts
@@ -1,3 +1,6 @@
+// I looked upon 'infer' keyword that recursively reverses tuple while maintaing the order of the type, but found it difficult
+// to implement it, I will study about 'infer' tomorrow more deeply and implement it in this problem.
+
 // Function to reverse the tuple
 function reverseTuple(tuple: any[]): any[] {
   return tuple.reverse();

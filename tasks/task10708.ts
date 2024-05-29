7b10e03929a5103530bc4ddf878a16a5b22d0fb2
Prasanna Agnihotri <prasannagnihotri24@gmail.com>
2024-05-29 10:19:41 +0530

    Update task10708.ts

--- tasks/task10708.ts
+++ tasks/task10708.ts
@@ -1,44 +1,39 @@
-// Description
-
-
 // const obj = { a: 1, b: 'hello', c: true };
 // const updated = updateObject(obj, 'b', 'world');
+// Function that takes object as first parameter and second parameter
+//  of take only keys of the object provided. Also value type should be 
+//  same as the original
+
+export{}
 
 
-// Function that takes object as first parameter and second parameter of take only keys of the object provided. Also value type should be same as the original
- 
-
-
-
-// function that updates a key in the passed object
-
-function updateObject(object : object, key : string, updateValue : string){
-    
-    // edge case to check if key actually exists or not
-    if(!(key in object)){
-        return (`${key} does not exist.`);
-    }
-    
-    // update the key
-    object[key] = updateValue;
-    return object;
-
+// Generic function to make sure that a object is passed, the key passed to be updated should be the key of the object
+// and the data to be updated must have the correct datatype.
+function updateObject<TObj, K extends keyof TObj>(obj: TObj, key: K, value: TObj[K]) {
+    obj[key] = value;
+    return obj;
 }
 
-
-let obj : {
+// create a type for the users object
+type UserObject = {
     a : number,
     b : string,
-    c : boolean } = 
-    {
-    a : 1,
+    c : boolean
+}
+
+// create a user object
+const u1 : UserObject =  {
+    a  : 1,
     b : 'hello',
     c : true
-    };
+}
+
+// function calling
+console.log(updateObject(u1,'c', false)); 
+console.log(updateObject(u1,'c', 'world')); // error
+
+console.log(updateObject(u1,'b','world')); 
+console.log(updateObject(u1,'b', 2134)); // error
 
 
-console.log(obj); // { a: 1, b: 'hello', c: true }
-
-console.log(updateObject(obj,'b','world')); // { a: 1, b: 'world', c: true }
-console.log(updateObject(obj,'e','world')); // e does not exist.
 
